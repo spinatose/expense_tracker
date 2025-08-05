a1a15a6329e73052c4af3e6d793a1ea1f95c4fd4
Scot Pfuntner <spinatose@gmail.com>
2025-08-05 07:00:27 -0500

    added theming to main and used in widgets in app

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:expense_tracker/presentation/screen/expenses_home.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(255, 96, 59, 181)
+  seedColor: Color.fromARGB(255, 78, 17, 140)
 );
 
 void main() {
@@ -17,7 +17,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Expenses Tracker',
-      theme: ThemeData().copyWith(colorScheme: kColorScheme),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 8.0,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kColorScheme.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 16.0,
+          ),
+          bodyMedium: TextStyle(
+            color: kColorScheme.onSecondaryContainer,
+          ),
+        ),
+      ),
       home: const ExpensesHome(
         // This home is the widget that will be displayed when the app starts.
         // You can change this to any widget you want, such as a custom home
